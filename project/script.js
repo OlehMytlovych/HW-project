14987a9b6d77c10e3b3601d8a07f5bd98a07b873
Oleh Mytlovych <bigbang007700@gmail.com>
2019-04-09 23:06:28 +0300

    Validation and income level added

--- project/script.js
+++ project/script.js
@@ -1,25 +1,38 @@
 
 const money = prompt("What is your budget for this month?", "");
 const time = prompt("Enter the date YYYY-MM-DD", "");
-const necessaryExpensesType1 = 
-prompt("what is the necessary expense in this month?", "");
-const necessaryExpensesSum1 = prompt("How much is it going to be?", "");
-const necessaryExpensesType2 = 
-prompt("what is the necessary expense in this month?", "");
-const necessaryExpensesSum2 = prompt("How much is it going to be?", "");
 
 const appData = {
   budget: money,
   timeData: time,
-  expenses: {
-    necessaryExpensesType1: necessaryExpensesSum1,
-    necessaryExpensesType2: necessaryExpensesSum2,
-  },
-  optionalExpenses: {
-
-  },
+  expenses: {},
+  optionalExpenses: {},
   income: [],
   savings: false
 };
 
-alert(appData.budget/30);
+for ( let i = 0 ; i < 2; i++ ){
+  let a = prompt("what is the necessary expense in this month?", "");
+  let b = +prompt("How much is it going to be?", "");
+
+  if ( isNaN((parseInt(a))) && (typeof(a) != null) && a != '' 
+    && a.length < 50 && (typeof(b) != null) && b != '' && !isNaN(b)) {
+    appData.expenses[a] = b;
+  } else {
+    i--;
+  }
+}
+
+appData.moneyPerDay = appData.budget/30;
+alert(`Everyday budget: ${appData.moneyPerDay}`);
+
+if (appData.moneyPerDay < 100) {
+  console.log('Low income');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+  console.log('Average income');
+} else if (appData.moneyPerDay > 2000) {
+  console.log('High income');
+} else {
+  console.log('An error has occured');
+}
+

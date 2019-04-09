75df0798dd2332e98921c0b9f8b310aac57df097
Oleh Mytlovych <bigbang007700@gmail.com>
2019-04-09 22:03:14 +0300

    The Money Manager project(lesson1)

--- project/script.js
+++ project/script.js
@@ -1,15 +1,19 @@
 
 const money = prompt("What is your budget for this month?", "");
 const time = prompt("Enter the date YYYY-MM-DD", "");
-const necessaryExpensesType = 
+const necessaryExpensesType1 = 
 prompt("what is the necessary expense in this month?", "");
-const necessaryExpensesSum = prompt("How much is it going to be?", "");
+const necessaryExpensesSum1 = prompt("How much is it going to be?", "");
+const necessaryExpensesType2 = 
+prompt("what is the necessary expense in this month?", "");
+const necessaryExpensesSum2 = prompt("How much is it going to be?", "");
 
 const appData = {
   budget: money,
   timeData: time,
   expenses: {
-    necessaryExpensesType: necessaryExpensesSum
+    necessaryExpensesType1: necessaryExpensesSum1,
+    necessaryExpensesType2: necessaryExpensesSum2,
   },
   optionalExpenses: {
 
@@ -18,4 +22,4 @@ const appData = {
   savings: false
 };
 
-alert(money/30);
+alert(appData.budget/30);

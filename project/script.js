e4a88b39f73780a1a73608300a02659a85eb5973
Oleh Mytlovych <bigbang007700@gmail.com>
2019-04-15 12:26:32 +0300

    Created functions start(), chooseExpenses, chooseOptionalExpenses(), detectDayBudget(), detectLevel() and checkSavings()

--- project/script.js
+++ project/script.js
@@ -1,38 +1,76 @@
-
-const money = prompt("What is your budget for this month?", "");
-const time = prompt("Enter the date YYYY-MM-DD", "");
-
-const appData = {
+let money, time, appData = {
   budget: money,
   timeData: time,
   expenses: {},
   optionalExpenses: {},
   income: [],
-  savings: false
+  savings: true
 };
 
-for ( let i = 0 ; i < 2; i++ ){
-  let a = prompt("what is the necessary expense in this month?", "");
-  let b = +prompt("How much is it going to be?", "");
+function start (){
+  money = prompt("What is your budget for this month?", "");
+  time = prompt("Enter the date YYYY-MM-DD", "");
 
-  if ( isNaN((parseInt(a))) && (typeof(a) != null) && a != '' 
-    && a.length < 50 && (typeof(b) != null) && b != '' && !isNaN(b)) {
-    appData.expenses[a] = b;
-  } else {
-    i--;
+  while (isNaN(money) || money == '' || money == null) { //check if money = null or on other words if user clicks "cancel" 
+    money = prompt("What is your budget for this month?", "");
   }
 }
+start();
 
-appData.moneyPerDay = appData.budget/30;
-alert(`Everyday budget: ${appData.moneyPerDay}`);
+function chooseExpenses() {
+  for ( let i = 0 ; i < 2; i++ ){
+    let a = prompt("what is the necessary expense in this month?", ""),
+       b = +prompt("How much is it going to be?", "");
 
-if (appData.moneyPerDay < 100) {
-  console.log('Low income');
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-  console.log('Average income');
-} else if (appData.moneyPerDay > 2000) {
-  console.log('High income');
-} else {
-  console.log('An error has occured');
+    if ( isNaN((parseInt(a))) && (typeof(a) != null) && a != '' 
+      && a.length < 50 && (typeof(b) != null) && b != '' && !isNaN(b)) {
+      appData.expenses[a] = b;
+    } else {
+     i--;
+    }
+  }
 }
+chooseExpenses();
+
+function chooseOptionalExpenes(){
+  for (let i = 1; i < 4; i++){
+    let newOptionalExp = prompt("Any optional expenses?", "");
+    if (newOptionalExp != '' && newOptionalExp != null){
+      appData.optionalExpenses[i] = newOptionalExp;
+    }
+  }
+  
+
+}
+chooseOptionalExpenes();
+
+function detectDayBudget(){
+  appData.moneyPerDay = (appData.budget/30).toFixed();//.toFixed() - to round number to integer, .toFixed(1) - one charachter after coma. STRING
+  alert(`Everyday budget: ${appData.moneyPerDay}`);
+}
+detectDayBudget()
+
+function detectLevel(){
+  if (appData.moneyPerDay < 100) {
+    console.log('Low income');
+  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('Average income');
+  } else if (appData.moneyPerDay > 2000) {
+    console.log('High income');
+  } else {
+    console.log('An error has occured');
+  }
+}
+detectLevel();
+
+function checkSavings(){
+  if (appData.savings == true){
+    let save = prompt("What is the sum of your savings", ''),
+        interest = prompt("What is the interest", "")
+
+    appData.monthIncome = save/100/12*interest;
+    alert(`Month income from your deposit is ${appData.monthIncome}`)
+  }
+}
+checkSavings();
 

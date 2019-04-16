366c75020127e73a62b1513f1bfed2b52dcb7ce0
Oleh Mytlovych <bigbang007700@gmail.com>
2019-04-16 18:42:26 +0300

    new html file of the money keeper app and main variables in the js file assigned

---
+++ project/src/js/main.js
@@ -0,0 +1,27 @@
+let beginCalButton = document.querySelector('#start'),
+
+    budgetValue = document.querySelector('.budget-value'),
+    dayBudgetValue = document.querySelector('.daybudget-value'),
+    levelValue = document.querySelector('.level-value'),
+    expensesValue = document.querySelector('.expenses-value'),
+    incomeValue = document.querySelector('.income-value'),
+    optionalExpensesValue = document.querySelector('.optionalexpenses-value'),
+    monthSavingsValue = document.querySelector('.monthsavings-value'),
+    yearSavingsValue = document.querySelector('.yearsavings-value'),
+
+    expensesInputs = document.querySelectorAll('.expenses-item'),
+    submitExpenses = document.querySelector('.expenses-item-btn'),
+
+    optionalExpensesInputs = document.querySelectorAll('.optionalexpenses-item'),
+    submitOptionalExpenses = document.querySelector('.optionalexpenses-btn'),
+
+    chooseIncomeInput = document.querySelector('#income'),
+    savingsCheckbox = document.querySelector('#savings'),
+    savingsSum = document.querySelector('#sum'),
+    savingsInterest = document.querySelector('#percent'),
+
+    year = document.querySelector('.year-value'),
+    month = document.querySelector('.month-value'),
+    day = document.querySelector('.day-value');
+
+    
